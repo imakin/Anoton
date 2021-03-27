11332acaee89af99d83a2108b135071495fb6dd0
Muhammad Izzulmakin <makin.ugm@gmail.com>
2021-03-27 21:44:14 +0700

    argon2.js structure changed

--- argon2/argon2-loader.js
+++ argon2/argon2-loader.js
@@ -69,9 +69,9 @@
             return global.loadArgon2WasmModule();
         }
         if (typeof require === 'function') {
-            return Promise.resolve(require('../dist/argon2.js'));
+            return Promise.resolve(require('./argon2.js'));
         }
-        return import('../dist/argon2.js');
+        return import('./argon2.js');
     }
 
     function loadWasmBinary() {
@@ -79,7 +79,7 @@
             return global.loadArgon2WasmBinary();
         }
         if (typeof require === 'function') {
-            return Promise.resolve(require('../dist/argon2.wasm')).then(
+            return Promise.resolve(require('./argon2.wasm')).then(
                 (wasmModule) => {
                     return decodeWasmBinary(wasmModule);
                 }
@@ -87,7 +87,7 @@
         }
         const wasmPath =
             global.argon2WasmPath ||
-            'node_modules/argon2-browser/dist/argon2.wasm';
+            'argon2/argon2.wasm';
         return fetch(wasmPath)
             .then((response) => response.arrayBuffer())
             .then((ab) => new Uint8Array(ab));
